fd20d05be6cf29866805b167421c7d274a7438ed
Isha Bali <ishabali@gmail.com>
2020-09-23 15:25:43 -0500

    Home page text changed

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -17,15 +17,28 @@ function Home() {
         </Row>
         <Row>
           <Col size="md-12">
+            <p>I am a Full Stack Web Developer, leveraging 6 years of experience
+             in IT to build beautiful, multi-featured, responsive websites building
+              brand visibility and business functionality. I am an MCA (Master in Computer 
+              Applications) and have previous work experience in System design, development and 
+              implementation in Powerbuilder and Oracle. </p>
+            <p>Recently I completed a full stack coding bootcamp from Northwestern 
+               University and developed new frontend and backend skills. My current field of 
+               interest is buildinf full-stack websites.   </p>
+            <p>I am a detail-focused, problem solving web developer with good communication
+              and teamwork skills, and a passion for coding. I want to further learn and grow
+               in an organization and contribute there by doing high quality work. </p>
             <p>
-            I am a Web Developer based in Chicago. I have a work experience of over 6 years as a programmer.
-            I am a Masters in Computer Applications. </p>
-           <p>
-            I have work-experience in development in Powerbuilder, Delphi and Oracle and in 
-            Project Design. My current area of interest is Web Development. 
-            I am a full-stack web developer.             
+              <strong>Technical Skills:</strong>
+              Languages : HTML, CSS, Javascript
+              Libraries : React, Bootstrap, jQuery
+              Databases : MySQL, MongoDB, Oracle
+              Frameworks: Node.js, Express
+              Tools     : Github, Heroku, AWS, Robo 3T, SQL Workbench, Powerbuilder             
             </p>
-            <a href="https://www.canva.com/design/DAEIkcy6wqU/3xs_etKHXsCQOtvBCLx1Yw/view?utm_content=DAEIkcy6wqU&utm_campaign=designshare&utm_medium=link&utm_source=sharebutton" target="_blank">Resume</a>
+            <strong>
+              <a href="https://www.canva.com/design/DAEIkcy6wqU/3xs_etKHXsCQOtvBCLx1Yw/view?utm_content=DAEIkcy6wqU&utm_campaign=designshare&utm_medium=link&utm_source=sharebutton" target="_blank">Resume</a>
+            </strong>
           </Col>
         </Row>
       </Container>
